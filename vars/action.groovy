ac29fddd2ae554dd082bf40289e30bb6a3ba7fb6
landmarkC30testing <118633121+landmarkC30testing@users.noreply.github.com>
2023-01-10 13:17:38 -0800

    Create action.groovy

---
+++ vars/action.groovy
@@ -0,0 +1,20 @@
+def call(String stageName){
+  
+  if ("${stageName}" == "Build")
+     {
+       sh "mvn clean package"
+     }
+  else if ("${stageName}" == "SonarQube Report")
+     {
+       sh "mvn clean sonar:sonar"
+     }
+  else if ("${stageName}" == "Upload Into Nexus")
+     {
+       sh "mvn deploy"
+     }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
